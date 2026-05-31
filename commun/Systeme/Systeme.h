9a08fdc81531c9d6f00fddd1c21a8ce9fcd29f3c
tancrède <tancredebohbot@gmail.com>
2026-05-31 23:28:19 +0200

    h

--- commun/Systeme/Systeme.h
+++ commun/Systeme/Systeme.h
@@ -14,7 +14,6 @@ class Systeme: public Dessinable {
     public:
     // constructeurs
         Systeme() : temps(0) {};
-        Systeme(unsigned int p, unsigned int o, unsigned int s) : temps(0) {particules.reserve(p);obstacles.reserve(o);sources.reserve(s);};
         Systeme(const std::vector<Particule*>& p, const std::vector<Obstacle*>& o, const std::vector<Source*>& s) : temps(0) {particules = p; obstacles = o; sources = s;};
         Systeme(double t, unsigned int p, unsigned int o, unsigned int s) : temps(t) {particules.reserve(p);obstacles.reserve(o);sources.reserve(s);};
         Systeme(const double& t) : temps(t) {};
